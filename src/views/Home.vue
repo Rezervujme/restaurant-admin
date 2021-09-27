66e001b9d6fa86e1381bf63d02b257d3aadfc61a
Jakub Jelínek <jakubkoje@gmail.com>
2021-09-27 21:39:54 +0200

    table editing and export

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,7 @@
       />
       <div class="mr-8 ml-4 flex-grow">
         <Button
+          class="!mr-4"
           icon="pi pi-plus"
           @click="addTable"
         />
@@ -24,35 +25,67 @@
           icon="pi pi-minus"
           @click="removeTable"
         />
-        <div class="bg-red-200 h-64">
+        <div class="bg-red-200 h-64 my-4">
           <div
             v-for="table in tables"
             :id="`table-${table.id}`"
             :key="table.id"
             class="h-8 w-8 bg-black"
+            :class="table.shape === 'circle' ? 'rounded-full' : ''"
             @click="openSettings($event, table.id)"
           />
           <OverlayPanel ref="op">
-            <img
-              src="https://via.placeholder.com/150"
-              alt="Nature Image"
-            >
-            <h1>{{ currentTable.id }}</h1>
-            <Button @click="test" />
+            <h1 class="text-2xl">
+              Stôl
+            </h1>
+            <div class="flex items-center">
+              <h5 class="text-lg mr-16">
+                Označenie:
+              </h5>
+              <InputText
+                v-model="currentTable.label"
+                class="ml-auto"
+              />
+            </div>
+            <div class="flex items-center">
+              <h5 class="text-lg mr-16">
+                Tvar:
+              </h5>
+              <Dropdown
+                v-model="currentTable.shape"
+                :options="shapes"
+                option-label="name"
+                option-value="code"
+                class="ml-auto"
+              />
+            </div>
           </OverlayPanel>
         </div>
+        <Button @click="exportData">
+          Export
+        </Button>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import {
+  computed, ref, toRaw,
+} from 'vue';
 import { PrimeIcons } from 'primevue/api';
 import interact from 'interactjs';
 import { v4 as UUIDv4 } from 'uuid';
 
-interface Table {id: string; test: number; position: {x:number; y:number}}
+interface Table {
+  id: string;
+  label: string;
+  shape: string;
+  position: {
+    x: number;
+    y: number
+  }
+}
 
 const tables = ref<Table[]>([]);
 
@@ -70,22 +103,11 @@ const items = ref([
     },
     ],
   },
-  {
-    label: 'Navigate',
-    items: [{
-      label: 'Vue Website',
-      icon: PrimeIcons.EXTERNAL_LINK,
-      url: 'https://vuejs.org/',
-    },
-    {
-      label: 'Router',
-      icon: PrimeIcons.UPLOAD,
-      command: () => {
-        window.location.hash = '/fileupload';
-      },
-    },
-    ],
-  },
+]);
+
+const shapes = ref([
+  { name: 'Štvorec', code: 'square' },
+  { name: 'Kruh', code: 'circle' },
 ]);
 
 function addTable() {
@@ -96,7 +118,8 @@ function addTable() {
       x: 0,
       y: 0,
     },
-    test: 0,
+    shape: 'square',
+    label: '',
   });
   interact(`#table-${id}`).draggable({
     modifiers: [
@@ -135,8 +158,8 @@ function openSettings(e: MouseEvent, tableId: string) {
 
 const currentTable = computed(() => tables.value.find((t) => t.id === selectedTableId.value));
 
-function test() {
-  if (!currentTable.value) return;
-  currentTable.value.test += 1;
+function exportData() {
+  const test = toRaw(tables.value);
+  console.log(test);
 }
 </script>
